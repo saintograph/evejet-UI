67c88f1be30fd442f0c39c915de26ba59b3cba71
winfredselwyn <winfredselwyn@gmail.com>
2017-02-22 15:13:29 +0800

    feat: Location autosuggest & maps
    Added Leaflet and Google Places API. Might remove Leaflet later and use Google-Maps instead for easier integration.

--- src/views/event-details.js
+++ src/views/event-details.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import { Card, Menu, Segment, Divider, Form, TextArea } from 'semantic-ui-react';
-import reactCSS from 'reactcss';
+import { Card, Divider } from 'semantic-ui-react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { StyleSheet, css } from 'aphrodite';
+import Geosuggest from 'react-geosuggest';
 import NavBar from '../components/navbar';
+import { slideDownReturn } from 'react-magic';
 
-const styles = reactCSS({
-  'default': {},
+const styles = StyleSheet.create({
+  magic: {
+    animationName: slideDownReturn,
+    animationDuration: '1s',
+  },
 });
+
+const position = [51.505, -0.09];
+
+const locationMap = () => {
+  return (
+    <Map center={position} zoom={16}>
+      <TileLayer
+        url='http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
+        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+      />
+      <Marker position={position}>
+        <Popup>
+          <span>Event here!</span>
+        </Popup>
+      </Marker>
+    </Map>
+  );
+};
 
 class EventDetails extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <div style={styles.cardContainer}>
+        <div style={styles.cardContainer} className={css(styles.magic)} >
           <div className="row center-xs">
             <div className="col-md-4 col-lg-3">
               <Card fluid style={styles.card}>
-                <h1>Test</h1>
+                <h1>Location and Guest List</h1>
+                <div>
+                  {locationMap()}
+                </div>
+                <Divider />
+                <div>
+                  <h3>Location</h3>
+                  <Geosuggest />
+                </div>
+                <Divider />
               </Card>
             </div>
           </div>
